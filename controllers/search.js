44ea3aa033083f8f6f30e46c9eaa6ae8de142e90
leonardo <clgrhc@gmail.com>
2021-05-21 03:56:10 -0500

    categories and products search

--- controllers/search.js
+++ controllers/search.js
@@ -23,6 +23,45 @@ const searchUsers = async (term = "", res) => {
   });
 };
 
+const searchCategories = async (term = "", res) => {
+  const isMongoId = ObjectId.isValid(term);
+
+  if (isMongoId) {
+    const category = await Category.findById(term);
+    return res.json({ results: category ? [category] : [] });
+  }
+
+  const regex = new RegExp(term, "i");
+
+  const categories = await Category.find({
+    $and: [{ name: regex }, { status: true }],
+  });
+
+  res.json({
+    results: categories,
+  });
+};
+
+const searchProducts = async (term = "", res) => {
+  const isMongoId = ObjectId.isValid(term);
+
+  if (isMongoId) {
+    const product = await Product.findById(term).populate("category", "name");
+    return res.json({ results: product ? [product] : [] });
+  }
+
+  const regex = new RegExp(term, "i");
+
+  const products = await Product.find({
+    $or: [{ name: regex }, { description: regex }],
+    $and: [{ status: true }],
+  }).populate("category", "name");
+
+  res.json({
+    results: products,
+  });
+};
+
 const search = (req, res) => {
   const { collection, term } = req.params;
 
@@ -37,8 +76,10 @@ const search = (req, res) => {
       searchUsers(term, res);
       break;
     case "categories":
+      searchCategories(term, res);
       break;
     case "products":
+      searchProducts(term, res);
       break;
     default:
       res.status(500).json({
